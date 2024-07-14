9d4e5a1d756d7588ccb7e7f274b4476ae58b3684
benouarAbdou <benouarabdelouahab@gmail.com>
2024-07-14 14:45:29 +0200

    chat mecanisme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
@@ -34,19 +33,42 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   final TextEditingController _controller = TextEditingController();
-  Uint8List? _imageData;
+  final ScrollController _scrollController = ScrollController();
+  final List<Message> _messages = [];
   bool _loading = false;
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _scrollToBottom() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+  }
 
   Future<void> generateImage(String text) async {
     setState(() {
       _loading = true;
+      _messages.add(Message(text: text, isUser: true));
+      _messages
+          .add(Message(text: "We are treating your prompt...", isUser: false));
     });
+    _scrollToBottom();
 
     try {
       String engineId = "stable-diffusion-v1-6";
       String apiHost = 'https://api.stability.ai';
-      String apiKey =
-          "sk-c0sd5OkEX9B7bTjtMUhdVtAkdxhBCvM2Txipin4jRy9Rtuiy"; // Replace with your actual API key
+      String apiKey = "sk-c0sd5OkEX9B7bTjtMUhdVtAkdxhBCvM2Txipin4jRy9Rtuiy";
 
       final response = await http.post(
         Uri.parse("$apiHost/v1/generation/$engineId/text-to-image"),
@@ -55,99 +77,138 @@ class _HomePageState extends State<HomePage> {
           "Accept": "image/png",
           "Authorization": "Bearer $apiKey"
         },
-        body: jsonEncode(
-          {
-            "text_prompts": [
-              {
-                "text": text,
-                "weight": 1,
-              }
-            ],
-            "cfg_scale": 7,
-            "height": 1024,
-            "width": 1024,
-            "samples": 1,
-            "steps": 30,
-          },
-        ),
+        body: jsonEncode({
+          "text_prompts": [
+            {
+              "text": text,
+              "weight": 1,
+            }
+          ],
+          "cfg_scale": 7,
+          "height": 512,
+          "width": 512,
+          "samples": 1,
+          "steps": 30,
+        }),
       );
 
       if (response.statusCode == 200) {
         setState(() {
-          _imageData = response.bodyBytes;
+          _messages.removeLast(); // Remove the loading message
+          _messages.add(Message(
+              text: "AI Generated Image",
+              isUser: false,
+              imageData: response.bodyBytes));
           _loading = false;
         });
-
-        // Save image to file
       } else {
         setState(() {
+          _messages.removeLast(); // Remove the loading message
+          _messages.add(Message(
+              text: "Failed to generate image: ${response.statusCode}",
+              isUser: false));
           _loading = false;
         });
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-              content:
-                  Text('Failed to generate image: ${response.statusCode}')),
-        );
       }
     } catch (e) {
       setState(() {
+        _messages.removeLast(); // Remove the loading message
+        _messages
+            .add(Message(text: "Error generating image: $e", isUser: false));
         _loading = false;
       });
-      print('Error generating image: $e');
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Error generating image: $e')),
-      );
     }
+    _scrollToBottom();
   }
 
-//#
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            const SizedBox(height: 20),
-            _loading
-                ? const CircularProgressIndicator()
-                : _imageData != null
-                    ? ClipRRect(
-                        borderRadius: BorderRadius.circular(25),
-                        child: Image.memory(_imageData!))
-                    : Container(),
-            const SizedBox(
-              height: 10,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const SizedBox(height: 20),
+          Expanded(
+            child: ListView.builder(
+              controller: _scrollController,
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return Align(
+                  alignment: message.isUser
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Container(
+                    margin:
+                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                    decoration: BoxDecoration(
+                      color: message.isUser
+                          ? const Color(0xFF587AF6)
+                          : const Color(0xFFf0f5fe),
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(20),
+                        topRight: const Radius.circular(20),
+                        bottomLeft: message.isUser
+                            ? const Radius.circular(20)
+                            : const Radius.circular(0),
+                        bottomRight: message.isUser
+                            ? const Radius.circular(0)
+                            : const Radius.circular(20),
+                      ),
+                    ),
+                    child: message.imageData != null
+                        ? ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: Image.memory(message.imageData!,
+                                width: 200, height: 200),
+                          )
+                        : Padding(
+                            padding: const EdgeInsets.symmetric(
+                                horizontal: 18, vertical: 12),
+                            child: Text(
+                              message.text,
+                              style: TextStyle(
+                                color: message.isUser
+                                    ? Colors.white
+                                    : Colors.black,
+                              ),
+                            ),
+                          ),
+                  ),
+                );
+              },
             ),
-            Row(
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Row(
               children: [
                 Expanded(
                   child: TextField(
                     controller: _controller,
+                    maxLines: 3,
+                    minLines: 1,
                     decoration: InputDecoration(
-                      hintText: "Enter a prompt ",
-                      hintStyle: const TextStyle(
-                          color: Colors.grey), // Hint text color
-                      filled: true, // To make the background filled
-                      fillColor: const Color(0xFFf0f5fe), // Background color
+                      hintText: "Enter a prompt",
+                      hintStyle: const TextStyle(color: Colors.grey),
+                      filled: true,
+                      fillColor: const Color(0xFFf0f5fe),
                       border: OutlineInputBorder(
-                        borderRadius:
-                            BorderRadius.circular(25.0), // Rounded corners
-                        borderSide: BorderSide.none, // No border
+                        borderRadius: BorderRadius.circular(25.0),
+                        borderSide: BorderSide.none,
                       ),
                       contentPadding: const EdgeInsets.symmetric(
-                          vertical: 10.0,
-                          horizontal: 20.0), // Padding inside the field
+                          vertical: 10.0, horizontal: 20.0),
                     ),
                   ),
                 ),
-                const SizedBox(
-                  width: 10,
-                ),
+                const SizedBox(width: 10),
                 GestureDetector(
                   onTap: () {
-                    generateImage(_controller.text);
+                    if (_controller.text.isNotEmpty) {
+                      generateImage(_controller.text);
+                      _controller.clear();
+                    }
                   },
                   child: Container(
                     padding: const EdgeInsets.all(10),
@@ -161,9 +222,17 @@ class _HomePageState extends State<HomePage> {
                 )
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+}
+
+class Message {
+  final String text;
+  final bool isUser;
+  final Uint8List? imageData;
+
+  Message({required this.text, required this.isUser, this.imageData});
 }
