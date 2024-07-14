ee3e61d9e534bce5ab69aabf044f9a49b5389369
benouarAbdou <benouarabdelouahab@gmail.com>
2024-07-14 18:59:36 +0200

    ui update

--- lib/database/myDataBase.dart
+++ lib/database/myDataBase.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
@@ -19,15 +21,10 @@ class SqlDb {
     Database myDb = await openDatabase(
       path,
       onCreate: _onCreate,
-      version: 1,
-      onUpgrade: _onUpgrade,
+      version: 2,
       onOpen: _onOpen,
     );
     return myDb;
-  }
-
-  Future<void> _onUpgrade(Database db, int oldVersion, int newVersion) async {
-    print("onUpgrade =====================================");
   }
 
   Future<void> _onOpen(Database db) async {
@@ -37,20 +34,15 @@ class SqlDb {
   Future<void> _onCreate(Database db, int version) async {
     await db.execute("PRAGMA foreign_keys = ON");
 
-    await db.execute('''
-CREATE TABLE "recipes" (
-  "recipeId" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-  "name" TEXT NOT NULL,
-  "youtubeLink" TEXT,
-  "difficulty" TEXT,
-  "Ingredients" TEXT,
-  "directions" TEXT,
-  "type" TEXT,
-  "path" TEXT,
-  "durationInMinutes" INTEGER
+    await db.execute(
+        '''
+CREATE TABLE "messages" (
+  "id" INTEGER PRIMARY KEY AUTOINCREMENT,
+  "text" TEXT NOT NULL,
+  "isUser" INTEGER NOT NULL,
+  "imageData" BLOB,
+  "prompt" TEXT
 )''');
-
-    print("Tables created: recipes, ingredients, directions");
   }
 
   Future<List<Map<String, dynamic>>> readData(String sql) async {
@@ -75,5 +67,59 @@ CREATE TABLE "recipes" (
     Database? myDb = await db;
     int response = await myDb!.rawDelete(sql);
     return response;
+  }
+
+  Future<int> insertMessage(Message message) async {
+    Database? myDb = await db;
+    return await myDb!.insert('messages', message.toMap());
+  }
+
+  Future<List<Message>> getMessages() async {
+    Database? myDb = await db;
+    final List<Map<String, dynamic>> maps = await myDb!.query('messages');
+    return List.generate(maps.length, (i) {
+      return Message.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> clearMessages() async {
+    Database? myDb = await db;
+    await myDb!.delete('messages');
+  }
+}
+
+class Message {
+  final int? id;
+  final String text;
+  final bool isUser;
+  final Uint8List? imageData;
+  final String? prompt;
+
+  Message({
+    this.id,
+    required this.text,
+    required this.isUser,
+    this.imageData,
+    this.prompt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'text': text,
+      'isUser': isUser ? 1 : 0,
+      'imageData': imageData,
+      'prompt': prompt,
+    };
+  }
+
+  factory Message.fromMap(Map<String, dynamic> map) {
+    return Message(
+      id: map['id'],
+      text: map['text'],
+      isUser: map['isUser'] == 1,
+      imageData: map['imageData'],
+      prompt: map['prompt'],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,10 @@ import 'dart:convert';
 import 'dart:typed_data';
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_generator/database/myDataBase.dart';
 import 'package:media_scanner/media_scanner.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
@@ -20,6 +21,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'AI Image Generator',
       theme: ThemeData(
+        fontFamily: 'Folks',
         useMaterial3: false,
       ),
       home: const HomePage(),
@@ -39,6 +41,27 @@ class _HomePageState extends State<HomePage> {
   final ScrollController _scrollController = ScrollController();
   final List<Message> _messages = [];
   bool _loading = false;
+  final SqlDb _sqlDb = SqlDb();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMessages();
+    _scrollToBottom();
+  }
+
+  Future<void> _loadMessages() async {
+    final messages = await _sqlDb.getMessages();
+    setState(() {
+      _messages.clear(); // Clear existing messages
+      _messages.addAll(messages); // Add loaded messages
+    });
+    _scrollToBottom();
+  }
+
+  Future<void> _saveMessage(Message message) async {
+    await _sqlDb.insertMessage(message);
+  }
 
   @override
   void dispose() {
@@ -59,21 +82,52 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Map<String, int> getDimensions(String text) {
+    int width = 512;
+    int height = 512;
+    int factor = 1;
+    //if (text.toLowerCase().contains("hd")) factor = 2;
+
+    if (text.toLowerCase().contains("portrait")) {
+      width = 512 * factor;
+      height = 1024 * factor;
+    } else if (text.toLowerCase().contains("landscape")) {
+      width = 1024 * factor;
+      height = 512 * factor;
+    } else if (text.toLowerCase().contains("square")) {
+      width = 512 * factor;
+      height = 512 * factor;
+    } else if (text.toLowerCase().contains("default")) {
+      width = 512 * factor;
+      height = 512 * factor;
+    }
+
+    return {
+      "width": width,
+      "height": height,
+    };
+  }
+
   Future<void> generateImage(String text, {bool isRegenerate = false}) async {
     setState(() {
       _loading = true;
       if (!isRegenerate) {
-        _messages.add(Message(text: text, isUser: true));
+        final userMessage = Message(text: text, isUser: true);
+        _messages.add(userMessage);
+        _saveMessage(userMessage);
       }
-      _messages
-          .add(Message(text: "We are treating your prompt...", isUser: false));
+      final loadingMessage =
+          Message(text: "We are treating your prompt...", isUser: false);
+      _messages.add(loadingMessage);
     });
     _scrollToBottom();
 
     try {
       String engineId = "stable-diffusion-v1-6";
       String apiHost = 'https://api.stability.ai';
-      String apiKey = "sk-c0sd5OkEX9B7bTjtMUhdVtAkdxhBCvM2Txipin4jRy9Rtuiy";
+      String apiKey = "api key";
+
+      final dimensions = getDimensions(text);
 
       final response = await http.post(
         Uri.parse("$apiHost/v1/generation/$engineId/text-to-image"),
@@ -90,8 +144,8 @@ class _HomePageState extends State<HomePage> {
             }
           ],
           "cfg_scale": 7,
-          "height": 512,
-          "width": 512,
+          "height": dimensions["height"],
+          "width": dimensions["width"],
           "samples": 1,
           "steps": 30,
         }),
@@ -100,27 +154,34 @@ class _HomePageState extends State<HomePage> {
       if (response.statusCode == 200) {
         setState(() {
           _messages.removeLast(); // Remove the loading message
-          _messages.add(Message(
+          final generatedMessage = Message(
               text: "AI Generated Image",
               isUser: false,
               imageData: response.bodyBytes,
-              prompt: text));
+              prompt: text);
+          _messages.add(generatedMessage);
+          _saveMessage(generatedMessage);
           _loading = false;
         });
       } else {
         setState(() {
           _messages.removeLast(); // Remove the loading message
-          _messages.add(Message(
+          final errorMessage = Message(
               text: "Failed to generate image: ${response.statusCode}",
-              isUser: false));
+              isUser: false);
+          _messages.add(errorMessage);
           _loading = false;
+          _saveMessage(errorMessage);
         });
       }
     } catch (e) {
       setState(() {
         _messages.removeLast(); // Remove the loading message
-        _messages
-            .add(Message(text: "Error generating image: $e", isUser: false));
+        final errorMessage =
+            Message(text: "Error generating image: $e", isUser: false);
+        _messages.add(errorMessage);
+        _saveMessage(errorMessage);
+
         _loading = false;
       });
     }
@@ -185,141 +246,168 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          const SizedBox(height: 20),
-          Expanded(
-            child: ListView.builder(
-              controller: _scrollController,
-              itemCount: _messages.length,
-              itemBuilder: (context, index) {
-                final message = _messages[index];
-                final isLastImage =
-                    index == _messages.length - 1 && message.imageData != null;
-                return Align(
-                  alignment: message.isUser
-                      ? Alignment.centerRight
-                      : Alignment.centerLeft,
-                  child: Container(
-                    margin:
-                        const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                    decoration: BoxDecoration(
-                      color: message.isUser
-                          ? const Color(0xFF587AF6)
-                          : const Color(0xFFf0f5fe),
-                      borderRadius: BorderRadius.only(
-                        topLeft: const Radius.circular(20),
-                        topRight: const Radius.circular(20),
-                        bottomLeft: message.isUser
-                            ? const Radius.circular(20)
-                            : const Radius.circular(0),
-                        bottomRight: message.isUser
-                            ? const Radius.circular(0)
-                            : const Radius.circular(20),
-                      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              height: 60,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                            height: 35,
+                            width: 35,
+                            child: Image.asset('assets/robot_chat.png')),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        const Text(
+                          "Ai image genertaor",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        )
+                      ],
                     ),
-                    child: message.imageData != null
-                        ? Column(
-                            children: [
-                              GestureDetector(
-                                onTap: () =>
-                                    _showFullScreenImage(message.imageData!),
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(10),
-                                  child: Image.memory(message.imageData!,
-                                      width: 200, height: 200),
+                    IconButton(
+                        onPressed: () async {
+                          await _sqlDb.clearMessages();
+                          setState(() {
+                            _messages.clear();
+                          });
+                        },
+                        icon: const Icon(Icons.delete))
+                  ]),
+            ),
+            Expanded(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                padding: const EdgeInsets.symmetric(vertical: 0),
+                controller: _scrollController,
+                itemCount: _messages.length,
+                itemBuilder: (context, index) {
+                  final message = _messages[index];
+                  final isLastImage = index == _messages.length - 1 &&
+                      message.imageData != null;
+                  return Align(
+                    alignment: message.isUser
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 8, horizontal: 16),
+                      decoration: BoxDecoration(
+                        color: message.isUser
+                            ? const Color(0xFF587AF6)
+                            : const Color(0xFFf0f5fe),
+                        borderRadius: BorderRadius.only(
+                          topLeft: const Radius.circular(20),
+                          topRight: const Radius.circular(20),
+                          bottomLeft: message.isUser
+                              ? const Radius.circular(20)
+                              : const Radius.circular(0),
+                          bottomRight: message.isUser
+                              ? const Radius.circular(0)
+                              : const Radius.circular(20),
+                        ),
+                      ),
+                      child: message.imageData != null
+                          ? Column(
+                              children: [
+                                GestureDetector(
+                                  onTap: () =>
+                                      _showFullScreenImage(message.imageData!),
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.memory(
+                                      message.imageData!,
+                                      width: 200,
+                                    ),
+                                  ),
+                                ),
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    IconButton(
+                                      icon: const Icon(Icons.save),
+                                      onPressed: () =>
+                                          saveImage(message.imageData!),
+                                    ),
+                                    if (isLastImage)
+                                      IconButton(
+                                        icon: const Icon(Icons.refresh),
+                                        onPressed: () => generateImage(
+                                            message.prompt!,
+                                            isRegenerate: true),
+                                      ),
+                                  ],
+                                ),
+                              ],
+                            )
+                          : Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 18, vertical: 12),
+                              child: Text(
+                                message.text,
+                                style: TextStyle(
+                                  color: message.isUser
+                                      ? Colors.white
+                                      : Colors.black,
                                 ),
                               ),
-                              Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  IconButton(
-                                    icon: const Icon(Icons.save),
-                                    onPressed: () =>
-                                        saveImage(message.imageData!),
-                                  ),
-                                  if (isLastImage)
-                                    IconButton(
-                                      icon: const Icon(Icons.refresh),
-                                      onPressed: () => generateImage(
-                                          message.prompt!,
-                                          isRegenerate: true),
-                                    ),
-                                ],
-                              ),
-                            ],
-                          )
-                        : Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 18, vertical: 12),
-                            child: Text(
-                              message.text,
-                              style: TextStyle(
-                                color: message.isUser
-                                    ? Colors.white
-                                    : Colors.black,
-                              ),
                             ),
-                          ),
-                  ),
-                );
-              },
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    maxLines: 3,
-                    minLines: 1,
-                    decoration: InputDecoration(
-                      hintText: "Enter a prompt",
-                      hintStyle: const TextStyle(color: Colors.grey),
-                      filled: true,
-                      fillColor: const Color(0xFFf0f5fe),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(25.0),
-                        borderSide: BorderSide.none,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      controller: _controller,
+                      maxLines: 3,
+                      minLines: 1,
+                      decoration: InputDecoration(
+                        hintText: "Enter a prompt",
+                        hintStyle: const TextStyle(color: Colors.grey),
+                        filled: true,
+                        fillColor: const Color(0xFFf0f5fe),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: BorderSide.none,
+                        ),
+                        contentPadding: const EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
                       ),
-                      contentPadding: const EdgeInsets.symmetric(
-                          vertical: 10.0, horizontal: 20.0),
                     ),
                   ),
-                ),
-                const SizedBox(width: 10),
-                GestureDetector(
-                  onTap: () {
-                    if (_controller.text.isNotEmpty) {
-                      generateImage(_controller.text);
-                      _controller.clear();
-                    }
-                  },
-                  child: Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: const BoxDecoration(
-                        color: Color(0xFF587AF6), shape: BoxShape.circle),
-                    child: const Icon(Icons.add, color: Colors.white),
-                  ),
-                )
-              ],
+                  const SizedBox(width: 10),
+                  GestureDetector(
+                    onTap: () {
+                      if (_controller.text.isNotEmpty) {
+                        generateImage(_controller.text);
+                        _controller.clear();
+                      }
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                          color: Color(0xFF587AF6), shape: BoxShape.circle),
+                      child: const Icon(Icons.send, color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
-}
-
-class Message {
-  final String text;
-  final bool isUser;
-  final Uint8List? imageData;
-  final String? prompt;
-
-  Message(
-      {required this.text, required this.isUser, this.imageData, this.prompt});
 }
